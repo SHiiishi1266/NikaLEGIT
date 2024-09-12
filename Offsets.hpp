7498d96cc259a2b675cc3f9eba6e4867a874f32a
SHiiishi1266 <pacientenormaljob40@proton.me>
2024-09-12 13:50:49 -0300

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -5,13 +5,13 @@ constexpr long OFF_LEVEL = 0x182c554;       //[Miscellaneous]->LevelName
 constexpr long OFF_LOCAL_PLAYER = 0x2429238;//[Miscellaneous]->LocalPlayer
 constexpr long OFF_ENTITY_LIST = 0x1f55fb8; //[Miscellaneous]->cl_entitylist
 constexpr long OFF_GLOBAL_VARS = 0x182c050; //[Miscellaneous]->GlobalVars
-constexpr long OFF_NAMELIST = 0xd3cabd0;    //[Miscellaneous]->NameList
+constexpr long OFF_NAMELIST = 0xd3c9fd0;    //[Miscellaneous]->NameList
 
 // Buttons
 constexpr long OFF_IN_ATTACK = 0x0769b658;  //[Buttons]->in_attack
 constexpr long OFF_IN_JUMP = 0x0769b778;    //[Buttons]->in_jump
 constexpr long OFF_IN_DUCK = 0x0769b868;    //[Buttons]->in_duck
-constexpr long OFFSET_IN_FORWARD = 0x0769b5a8; //[Buttons]->in_forward
+constexpr long OFFSET_IN_FORWARD = 0x076dd128; //[Buttons]->in_forward
 constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2b70; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
 constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2b6c; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
 constexpr long OFFSET_TRAVERSAL_RELEASE_TIME = 0x2b78; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalReleaseTime
@@ -20,7 +20,7 @@ constexpr long OFFSET_WALL_RUN_CLEAR_TIME = 0x3618; //RecvTable.DT_LocalPlayerEx
 
 // Player
 constexpr long OFF_VIEW_MATRIX = 0x11a350;   //[RecvTable.DT_Player]->ViewMatrix
-constexpr long OFF_VIEW_RENDER = 0x76ddcd8;  //[RecvTable.DT_Player]->ViewRender
+constexpr long OFF_VIEW_RENDER = 0x76dd0d8;  //[RecvTable.DT_Player]->ViewRender
 constexpr long OFF_VIEW_MODELS = 0x2d98;     //[RecvTable.DT_Player]->m_hViewModels
 constexpr long OFF_ZOOMING = 0x1be1;         //[RecvTable.DT_Player]->m_bZooming
 constexpr long OFF_LOCAL_ORIGIN = 0x017c;    //[DataMap.CBaseViewModel]->m_vecAbsOrigin
@@ -47,7 +47,7 @@ constexpr long OFF_YAW = 0x223c - 0x8;;      //[DataMap.C_Player]=>m_currentFram
 constexpr long OFF_NAMEINDEX = 0x38;         //[RecvTable.DT_BaseEntity]
 constexpr long OFF_XPLEVEL = 0x3724;         //m_xp
 constexpr long OFF_SQUAD_ID = 0x0344;        //m_squadID
-constexpr long OFF_GAMEMODE = 0x2461290;    //mp_gamemode
+constexpr long OFF_GAMEMODE = 0x02461290;    //mp_gamemode
 constexpr long OFF_SPECTATOR_LIST = 0x1f17fa8; //IDA signtature -> [48 8B 0D ? ? ? ? 48 85 C9 74 ? 48 8B 01 FF ? ? 48 85 C0 74 ? 48 63 4E 38]
 constexpr long OFF_SPECTATOR_LIST_ARRAY = 0x97c; 
 
